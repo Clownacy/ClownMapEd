a2de10fd058a0d4f8a741cefe6136c9993fa2bbc
Clownacy <Clownacy@users.noreply.github.com>
2023-03-21 20:12:58 +0000

    Remove Windows dark mode "support".
    Qt's dark mode support on Windows is terrible and basically doesn't
    work.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 
 int main(int argc, char *argv[])
 {
-	// Enable dark theme on Windows.
-	// TODO: Check if the dark theme is enabled on Linux.
-	qputenv("QT_QPA_PLATFORM", "windows:darkmode=2");
-
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
