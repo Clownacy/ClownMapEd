23722f285a1d199cb2b93ce5ea59469ec8d4ea34
Clownacy <Clownacy@users.noreply.github.com>
2023-04-03 17:20:19 +0100

    Make sprite outline follow shape of sprite.
    Previously, this program would only draw a rectangle around the
    sprite, but this doesn't match SonMapEd, which specifically follows
    the edges of each sprite piece. This commit fixes that.

--- sprite-viewer.cpp
+++ sprite-viewer.cpp
@@ -1,6 +1,7 @@
 #include "sprite-viewer.h"
 
 #include <QPainter>
+#include <QPainterPath>
 
 #include "utilities.h"
 
@@ -47,21 +48,66 @@ void SpriteViewer::paintEvent(QPaintEvent* const event)
 	// Draw outline around selected sprite.
 	const SpriteFrame &selected_sprite = frames[m_selected_sprite_index];
 	const QColor background_colour = palette().color(QPalette::Window);
-	const QRect outline_rect = m_selected_piece_index == -1 ? selected_sprite.rect() : selected_sprite.pieces[m_selected_piece_index].rect();
 
-	// Draw a solid colour outline.
-	// This appears to be the exact formula that SonMapEd uses for its selection box.
-	painter.setPen(QColor(background_colour.red() ^ 0xFF, background_colour.green() ^ 0xFF, background_colour.blue() / 2));
-	painter.drawRect(outline_rect);
-
-	// Draw some gaps into the outline, to make it resemble the one in SonMapEd.
 	QBrush brush;
-	brush.setStyle(Qt::BrushStyle::DiagCrossPattern);
-	brush.setColor(background_colour);
-	QPen pen;
-	pen.setBrush(brush);
-	painter.setPen(pen);
-	painter.drawRect(outline_rect);
+	brush.setColor(QColor(background_colour.red() ^ 0xFF, background_colour.green() ^ 0xFF, background_colour.blue() / 2));
+	brush.setStyle(Qt::BrushStyle::SolidPattern);
+
+	painter.setPen(Qt::NoPen);
+	painter.setBrush(brush);
+
+	auto process_frame_or_piece = [this](std::function<void(const SpritePiece &piece)> callback)
+	{
+		auto &frame = sprite_mappings.frames[selected_sprite_index()];
+
+		if (m_selected_piece_index != -1)
+		{
+			callback(frame.pieces[m_selected_piece_index]);
+		}
+		else
+		{
+			for (auto &piece : frame.pieces)
+				callback(piece);
+		}
+	};
+
+	QPainterPath combined_outline_path;
+
+	process_frame_or_piece(
+		[&combined_outline_path](const SpritePiece &piece)
+		{
+			// This algorithm is akward and weird, but it's all I can think of to recreate SonMapEd's outline effect.
+			for (int y = 0; y < piece.height; ++y)
+			{
+				for (int x = 0; x < piece.width; ++x)
+				{
+					// Scale everything by 2 because the coordinate system here is twice the normal resolution.
+					const QRect rect((piece.x + x * Tile::WIDTH) * 2, (piece.y + y * Tile::HEIGHT) * 2, Tile::WIDTH * 2, Tile::HEIGHT * 2);
+
+					QPainterPath inner_path;
+					inner_path.addRect(rect.marginsAdded(QMargins(1, 1, 1, 1)));
+
+					QPainterPath outer_path;
+					outer_path.addRect(rect.marginsRemoved(QMargins(1, 1, 1, 1)));
+
+					const QPainterPath tile_outline_path(inner_path - outer_path);
+
+					// Union minus intersection.
+					combined_outline_path = (combined_outline_path + tile_outline_path) - (combined_outline_path & tile_outline_path);
+				}
+			}
+		}
+	);
+
+	// Scale to double-resolution so that the outline isn't twice as thick as it should be.
+	transform.scale(0.5, 0.5);
+	painter.setTransform(transform);
+
+	painter.drawPath(combined_outline_path);
+
+	// Scale back to normal resolution.
+	transform.scale(2.0, 2.0);
+	painter.setTransform(transform);
 
 	// Draw selected sprite.
 	selected_sprite.draw(painter, tile_manager, starting_palette_line);
