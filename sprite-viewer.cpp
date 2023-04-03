5bc61b1c8181e5002c80882bbb892ce3ffb7111d
Clownacy <Clownacy@users.noreply.github.com>
2023-04-03 17:45:08 +0100

    Make sprite outline match SonMapEd even closer.
    This version is not misaligned by one pixel. I've also added a bunch
    of documentation explaining how the effect is achieved.

--- sprite-viewer.cpp
+++ sprite-viewer.cpp
@@ -45,7 +45,10 @@ void SpriteViewer::paintEvent(QPaintEvent* const event)
 	if (frames.size() == 0)
 		return;
 
-	// Draw outline around selected sprite.
+	//////////////////////////////////////////
+	// Draw outline around selected sprite. //
+	//////////////////////////////////////////
+
 	const SpriteFrame &selected_sprite = frames[m_selected_sprite_index];
 	const QColor background_colour = palette().color(QPalette::Window);
 
@@ -56,7 +59,8 @@ void SpriteViewer::paintEvent(QPaintEvent* const event)
 	painter.setPen(Qt::NoPen);
 	painter.setBrush(brush);
 
-	auto process_frame_or_piece = [this](std::function<void(const SpritePiece &piece)> callback)
+	// Helper to perform an operation using each selected sprite piece.
+	const auto process_selected_pieces = [this](std::function<void(const SpritePiece &piece)> callback)
 	{
 		auto &frame = sprite_mappings.frames[selected_sprite_index()];
 
@@ -71,43 +75,78 @@ void SpriteViewer::paintEvent(QPaintEvent* const event)
 		}
 	};
 
-	QPainterPath combined_outline_path;
-
-	process_frame_or_piece(
-		[&combined_outline_path](const SpritePiece &piece)
-		{
-			// This algorithm is akward and weird, but it's all I can think of to recreate SonMapEd's outline effect.
-			for (int y = 0; y < piece.height; ++y)
+	// Helper to perform an operation using each tile of each selected sprite piece.
+	const auto process_tiles_in_selected_pieces = [&process_selected_pieces](std::function<void(const QRect &rect)> callback)
+	{
+		process_selected_pieces(
+			[&callback](const SpritePiece &piece)
 			{
-				for (int x = 0; x < piece.width; ++x)
+				for (int y = 0; y < piece.height; ++y)
 				{
-					// Scale everything by 2 because the coordinate system here is twice the normal resolution.
-					const QRect rect((piece.x + x * Tile::WIDTH) * 2, (piece.y + y * Tile::HEIGHT) * 2, Tile::WIDTH * 2, Tile::HEIGHT * 2);
+					for (int x = 0; x < piece.width; ++x)
+					{
+						const QRect rect(piece.x + x * Tile::WIDTH, piece.y + y * Tile::HEIGHT, Tile::WIDTH, Tile::HEIGHT);
 
-					QPainterPath inner_path;
-					inner_path.addRect(rect.marginsAdded(QMargins(1, 1, 1, 1)));
-
-					QPainterPath outer_path;
-					outer_path.addRect(rect.marginsRemoved(QMargins(1, 1, 1, 1)));
-
-					const QPainterPath tile_outline_path(inner_path - outer_path);
-
-					// Union minus intersection.
-					combined_outline_path = (combined_outline_path + tile_outline_path) - (combined_outline_path & tile_outline_path);
+						callback(rect);
+					}
 				}
 			}
+		);
+	};
+
+	// This algorithm is awkward and weird, but it's all I can think of to recreate SonMapEd's outline effect.
+	// We create the outline with a `QPainterPath`, which undergoes various area subtractions and unions.
+	QPainterPath combined_outline_path;
+
+	process_tiles_in_selected_pieces(
+		[&combined_outline_path](const QRect &rect)
+		{
+			// The general idea is to draw a 2-pixel border around each tile.
+			// One pixel resides inside the bounds of the tile, and the other resides outside it.
+			// This border is 2 pixels thick so that it erases both the inner and outer borders of neighbouring tiles.
+			// To create this border, we create a rectangle that's slightly smaller than the tile,
+			// and subtract from it a rectangle that is slightly larger than the tile.
+			QPainterPath inner_path;
+			inner_path.addRect(rect.marginsAdded(QMargins(1, 1, 1, 1)));
+
+			QPainterPath outer_path;
+			outer_path.addRect(rect.marginsRemoved(QMargins(1, 1, 1, 1)));
+
+			const QPainterPath tile_outline_path(inner_path - outer_path);
+
+			// Now that we have our tile border, we combine it with the others.
+			// To erase the border of neighbouring tiles, we also subtract the intersection.
+			combined_outline_path = (combined_outline_path | tile_outline_path) - (combined_outline_path & tile_outline_path);
 		}
 	);
 
-	// Scale to double-resolution so that the outline isn't twice as thick as it should be.
-	transform.scale(0.5, 0.5);
-	painter.setTransform(transform);
+	// At this point, we have our desired sprite outline, however
+	// it is 2 pixels thick instead of 1, so we must correct this.
+	process_tiles_in_selected_pieces(
+		[&combined_outline_path](const QRect &rect)
+		{
+			// To reduce the thickness of the outline, we create another series of tile borders,
+			// but this time they are only 1 pixel thick, and reside exclusively outside of the tile's bounds.
+			QPainterPath inner_path;
+			inner_path.addRect(rect.marginsAdded(QMargins(1, 1, 1, 1)));
 
+			QPainterPath outer_path;
+			outer_path.addRect(rect);
+
+			const QPainterPath tile_outline_path(inner_path - outer_path);
+
+			// We then subtract the intersection of these tiles with the sprite outline.
+			// This erases the outer part of the outline, leaving it only a single pixel thick.
+			combined_outline_path -= combined_outline_path & tile_outline_path;
+		}
+	);
+
+	// The sprite outline is finally complete, so we can now draw it.
 	painter.drawPath(combined_outline_path);
 
-	// Scale back to normal resolution.
-	transform.scale(2.0, 2.0);
-	painter.setTransform(transform);
+	///////////////////
+	// Draw sprites. //
+	///////////////////
 
 	// Draw selected sprite.
 	selected_sprite.draw(painter, tile_manager, starting_palette_line);
