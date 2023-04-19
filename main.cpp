f1b1f002fdf9eb7eaf8af72c4103bc4fbf56f32b
Clownacy <Clownacy@users.noreply.github.com>
2023-04-19 10:45:22 +0100

    Add a TODO.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[])
 {
+	// TODO: I think Ewan told me that Qt6 does this by default, so ditch this code if/when we switch from Qt5 to that!
 	QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
