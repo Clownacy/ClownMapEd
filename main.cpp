dc1b25a31fe3f8b715cdc6a8eb3b37a230b29fca
Clownacy <Clownacy@users.noreply.github.com>
2024-04-09 23:45:41 +0100

    Make compaible with Qt <5.14.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@ int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 	// Qt6 does this by default
-	QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+	QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
 #endif
 
 	QApplication a(argc, argv);
