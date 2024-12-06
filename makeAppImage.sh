09201e8129166ca5381a205cebaca61949db367b
Clownacy <Clownacy@users.noreply.github.com>
2024-12-06 03:03:55 +0000

    Add a script for producing AppImage file.
    It's such a damn complicated thing to do...

---
+++ makeAppImage.sh
@@ -0,0 +1,11 @@
+mkdir makeAppImage-build
+if [ ! -f makeAppImage-build/linuxdeploy-x86_64.AppImage ]; then
+	wget -O makeAppImage-build/linuxdeploy-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy/releases/download/2.0.0-alpha-1-20241106/linuxdeploy-x86_64.AppImage
+fi
+chmod +x makeAppImage-build/linuxdeploy-x86_64.AppImage
+rm -r makeAppImage-build/AppDir
+mkdir makeAppImage-build/AppDir
+cmake -B makeAppImage-build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build makeAppImage-build --config Release --parallel $(nproc)
+DESTDIR=AppDir cmake --build makeAppImage-build --target install
+LINUXDEPLOY_OUTPUT_VERSION=v1.2 LDAI_UPDATE_INFORMATION="gh-releases-zsync|Clownacy|clownmaped|latest|ClownMapEd-*x86_64.AppImage.zsync" makeAppImage-build/linuxdeploy-x86_64.AppImage --appdir makeAppImage-build/AppDir --output appimage
