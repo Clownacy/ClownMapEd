a4a234e4cb4bcadb5c49acbe30f748c29426e5df
Clownacy <Clownacy@users.noreply.github.com>
2024-12-06 03:17:55 +0000

    Rename AppImage build directory.
    This way, it gets grouped with the usual build directory if file
    explorers.

--- makeAppImage.sh
+++ makeAppImage.sh
@@ -1,14 +1,14 @@
-mkdir makeAppImage-build
-if [ ! -f makeAppImage-build/linuxdeploy-x86_64.AppImage ]; then
-	wget -O makeAppImage-build/linuxdeploy-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy/releases/download/2.0.0-alpha-1-20241106/linuxdeploy-x86_64.AppImage
+mkdir build-makeAppImage
+if [ ! -f build-makeAppImage/linuxdeploy-x86_64.AppImage ]; then
+	wget -O build-makeAppImage/linuxdeploy-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy/releases/download/2.0.0-alpha-1-20241106/linuxdeploy-x86_64.AppImage
 	if [ $? -ne 0 ]; then
-		curl -L -o makeAppImage-build/linuxdeploy-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy/releases/download/2.0.0-alpha-1-20241106/linuxdeploy-x86_64.AppImage
+		curl -L -o build-makeAppImage/linuxdeploy-x86_64.AppImage https://github.com/linuxdeploy/linuxdeploy/releases/download/2.0.0-alpha-1-20241106/linuxdeploy-x86_64.AppImage
 	fi
 fi
-chmod +x makeAppImage-build/linuxdeploy-x86_64.AppImage
-rm -r makeAppImage-build/AppDir
-mkdir makeAppImage-build/AppDir
-cmake -B makeAppImage-build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
-cmake --build makeAppImage-build --config Release --parallel $(nproc)
-DESTDIR=AppDir cmake --build makeAppImage-build --target install
-LINUXDEPLOY_OUTPUT_VERSION=v1.2 LDAI_UPDATE_INFORMATION="gh-releases-zsync|Clownacy|clownmaped|latest|ClownMapEd-*x86_64.AppImage.zsync" makeAppImage-build/linuxdeploy-x86_64.AppImage --appdir makeAppImage-build/AppDir --output appimage
+chmod +x build-makeAppImage/linuxdeploy-x86_64.AppImage
+rm -r build-makeAppImage/AppDir
+mkdir build-makeAppImage/AppDir
+cmake -B build-makeAppImage -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build-makeAppImage --config Release --parallel $(nproc)
+DESTDIR=AppDir cmake --build build-makeAppImage --target install
+LINUXDEPLOY_OUTPUT_VERSION=v1.2 LDAI_UPDATE_INFORMATION="gh-releases-zsync|Clownacy|clownmaped|latest|ClownMapEd-*x86_64.AppImage.zsync" build-makeAppImage/linuxdeploy-x86_64.AppImage --appdir build-makeAppImage/AppDir --output appimage
